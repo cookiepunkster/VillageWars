7683954537bb923cea18557e5b548ec5f12595a7
mlcsobrevinas <igi.sobrevinas@gmail.com>
2015-09-30 20:59:07 +0800

    Changes in main file
    Deleted comments

--- src/mainFile/VillageWars.java
+++ src/mainFile/VillageWars.java
@@ -7,10 +7,10 @@ import gameLogic.conditions;
 public class VillageWars {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 		  MainGameFrame frame1 = new MainGameFrame();
-	//	  InitialFrame frame2 = new InitialFrame();
+
 		  
 		  frame1.setDefaultLookAndFeelDecorated(true);
 		  frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,12 +20,7 @@ public class VillageWars {
 		  
 		  conditions listener1 = new conditions(frame1);
 		  frame1.getButton().addActionListener(listener1);
-		//  frame1.getTextArea().addActionListener(listener1);
-		  
-		//   conditions listener2 = new conditions();
-		 // frame2.getButton1Barb().addActionListener(listener2);
-		  
-		//DUH.
+
 	}
 
 }
