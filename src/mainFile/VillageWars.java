265fc728464e7c8fbbf0aa35d43f28745eb9e895
jezzadiaz <diazjezza@gmail.com>
2015-09-30 19:57:28 +0800

    TRIAL
    this is something, Just to try.

--- src/mainFile/VillageWars.java
+++ src/mainFile/VillageWars.java
@@ -4,7 +4,7 @@ public class VillageWars {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+             System.out.println("Just checking if this is working");
 		
 		//DUH.
 	}
