ef5b6822611a0b1a93d45c39ce9b2b2dfa843050
cookiepunkster <jolly.ann.bilad@gmail.com>
2015-09-30 20:26:02 +0800

    Trial

--- src/mainFile/VillageWars.java
+++ src/mainFile/VillageWars.java
@@ -5,6 +5,7 @@ public class VillageWars {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("SUBLUME MO MUKHA MO JEZZA");
 		
 			System.out.println("Pakyu Jezza");
             System.out.println("YEHEEEEEEEEY!!!");
