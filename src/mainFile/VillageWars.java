f4f7652e1e8e6496790a1fa67a422aa4f4260d9d
mlcsobrevinas <igi.sobrevinas@gmail.com>
2015-09-30 20:41:03 +0800

    trial2

--- src/mainFile/VillageWars.java
+++ src/mainFile/VillageWars.java
@@ -5,8 +5,8 @@ public class VillageWars {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("SUBLUME MO MUKHA MO JEZZA");
-		
+		System.out.println("123456");
+		System.out.println("78910");
 			System.out.println("Pakyu Jezza");
             System.out.println("YEHEEEEEEEEY!!!");
 
